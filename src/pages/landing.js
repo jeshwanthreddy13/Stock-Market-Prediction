524411e20d84b4d30ad2ed2cfcd13f9fd5c1d160
srinidhi131 <srinidhi131@gmail.com>
2022-08-16 12:36:13 +0530

    Nav

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,9 +1,17 @@
 import {React, Component } from "react";
+import './landing.css'
 
 class LandingPage extends Component {
     render() {
-      return <div>Landing Page</div>;
-    }
+      return (
+      <div>
+        <ul>
+            <li><a href="home">Home</a></li>
+            <li><a href="about">About Us</a></li>
+            <li><a href="services">Services</a></li>
+        </ul>
+      </div>
+    )}
   }
 
 export default LandingPage;
