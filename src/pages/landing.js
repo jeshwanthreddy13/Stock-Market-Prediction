f4de64b0fb970908cb9a671478aac84372fe9370
srinidhi131 <srinidhi131@gmail.com>
2022-08-18 12:14:02 +0530

    CSS for nav

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,30 +2,24 @@ import {React, Component } from "react";
 import './landing.css'
 import{VictoryCandlestick} from 'victory';
 import {data} from './data_lp';
+import Nav from './nav'
 
 class LandingPage extends Component {
     render() {
       return (
         <div>
-      <div>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="about">About Us</a></li>
-                <li><a href="services">Services</a></li>
-                <button>Login</button>
-            </ul>
-      </div>
-      <div>
-        <VictoryCandlestick candleColors={{ positive: "#43BFC7", negative: "#F70D1A" }}
-        candleWidth={20}
-        style={{
-          data: {
-             fillOpacity: 0.7, stroke: "#c43a31", strokeWidth: 3
-          },
-        }}
-        data={data}/>
-      </div>
-      </div>
+          <Nav/>
+          <div>
+            <VictoryCandlestick candleColors={{ positive: "#43BFC7", negative: "#F70D1A" }}
+            candleWidth={20}
+            style={{
+              data: {
+                fillOpacity: 0.7, stroke: "#c43a31", strokeWidth: 3
+              },
+            }}
+            data={data}/>
+          </div>
+        </div>
     )}
   }
 
