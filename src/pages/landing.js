c7d3996dde488c9e987257674b5bf58f73082c50
srinidhi131 <srinidhi131@gmail.com>
2022-08-18 11:47:02 +0530

    Candlesticks for landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,9 +1,12 @@
 import {React, Component } from "react";
 import './landing.css'
+import{VictoryCandlestick} from 'victory';
+import {data} from './data_lp';
 
 class LandingPage extends Component {
     render() {
       return (
+        <div>
       <div>
             <ul>
                 <li><a href="/">Home</a></li>
@@ -11,6 +14,17 @@ class LandingPage extends Component {
                 <li><a href="services">Services</a></li>
                 <button>Login</button>
             </ul>
+      </div>
+      <div>
+        <VictoryCandlestick candleColors={{ positive: "#43BFC7", negative: "#F70D1A" }}
+        candleWidth={20}
+        style={{
+          data: {
+             fillOpacity: 0.7, stroke: "#c43a31", strokeWidth: 3
+          },
+        }}
+        data={data}/>
+      </div>
       </div>
     )}
   }
